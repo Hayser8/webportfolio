07f40280e22c415a8a79c8ecb858968e71783404
Hayser8 <gar22076@uvg.edu.gt>
2024-05-13 03:52:12 -0600

    se agrega el apartado de proyectos

--- portfolio-julio/tailwind.config.mjs
+++ portfolio-julio/tailwind.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};

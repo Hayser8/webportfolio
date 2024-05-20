801176117f862e9589f778d9224ea5a2118c8001
Hayser8 <gar22076@uvg.edu.gt>
2024-05-20 02:56:20 -0600

    se agregan par de animaciones y gradientes al techstack

--- portfolio-julio/tailwind.config.mjs
+++ portfolio-julio/tailwind.config.mjs
@@ -4,14 +4,37 @@ export default {
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        gradientStart: '#374151',
+        gradientEnd: '#1F2937',
+      },
+      boxShadow: {
+        custom: 'rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px',
+      },
       keyframes: {
+        slideInFromRight: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateX(100%)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(0)',
+          },
+        },
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
+        fadeInWithOpacity: {
+          "0%": { opacity: 0},
+          "100%": { opacity: 0.8 },
+        },
       },
       animation: {
         fadeIn: "fadeIn 3s ease-out",
+        fadeInWithOpacity: "fadeInWithOpacity 3s ease-out forwards",
+        slideInFromRight: 'slideInFromRight 1s ease-out forwards',
       },
     },
   },
